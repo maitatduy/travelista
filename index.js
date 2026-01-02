a1165489e86f60b5a7a8328e7a1e732af5c3abff
maitatduy <maitatduy.work@gmail.com>
2026-01-02 07:35:14 +0700

    chore: configure pug as view engine

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+
+// Cấu hình Express dùng Pug
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
