08606928f37f822d07b022933a425a24a1c5b06a
maitatduy <maitatduy.work@gmail.com>
2026-01-08 19:38:31 +0700

    chore: register home and cart routes

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 
 const tourRoutes = require("./tour.route");
+const homeRoutes = require("./home.route");
+const cartRoutes = require("./cart.route");
 
 router.use("/tours", tourRoutes);
+router.use("/cart", cartRoutes);
+router.use("/", homeRoutes);
 
 module.exports = router;
