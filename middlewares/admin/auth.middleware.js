2139e44bb1ab648889c5e0a2e535e2b817253c51
maitatduy <maitatduy.work@gmail.com>
2026-01-11 22:26:30 +0700

    feat: add admin JWT authentication middleware

---
+++ middlewares/admin/auth.middleware.js
@@ -0,0 +1,32 @@
+const {pathAdmin} = require("../../config/variable.config");
+const AccountAdmin = require("../../models/account-admin.model");
+const jwt = require("jsonwebtoken");
+
+module.exports.verifyToken = async (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            res.redirect(`/${pathAdmin}/account/login`);
+            return;
+        }
+
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        const {id, email} = decode;
+        const existAccount = await AccountAdmin.findOne({
+            _id: id,
+            email: email,
+            status: "active"
+        })
+
+        if (!existAccount) {
+            res.clearCookie("token");
+            res.redirect(`/${pathAdmin}/account/login`);
+            return;
+        }
+
+        next();
+    } catch (error) {
+        res.clearCookie("token");
+        res.redirect(`/${pathAdmin}/account/login`);
+    }
+}
