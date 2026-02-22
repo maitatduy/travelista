971d01e69fb5f67ef4241e4d72dd74f4b2a8b889
maitatduy <maitatduy.work@gmail.com>
2026-02-22 11:43:33 +0700

    feat: add overview statistics widgets

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,32 @@
+const AccountAdmin = require("../../models/account-admin.model");
+const Order = require("../../models/order.model");
+
 module.exports.dashboard = async (req, res) => {
-    res.render("admin/pages/dashboard", {
-        pageTitle: "Trang tổng quan"
+    // Section 1
+    const overview = {
+        totalAdmin: 0,
+        totalUser: 0,
+        totalOrder: 0,
+        totalPrice: 0,
+    };
+
+    overview.totalAdmin = await AccountAdmin.countDocuments({
+        deleted: false,
     });
-}
+
+    const orderList = await Order.find({
+        deleted: false,
+    });
+
+    overview.totalOrder = orderList.length;
+
+    overview.totalPrice = orderList.reduce((sum, item) => {
+        return sum + item.total;
+    }, 0);
+    // End Section 1
+
+    res.render("admin/pages/dashboard", {
+        pageTitle: "Tổng quan",
+        overview: overview,
+    });
+};
