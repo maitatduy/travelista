bf4a9da4d73aebe7d057f8b862a412f3c1bf6220
maitatduy <maitatduy.work@gmail.com>
2026-02-22 10:19:32 +0700

    feat: add cart page

--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const cartController = require("../../controllers/client/cart.controller");
 
 router.get("/", cartController.cart);
+router.post("/detail", cartController.detail);
 
 module.exports = router;
