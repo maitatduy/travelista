b3e6bfe90e516e35fd6f85b6e8d0a6a0e9a0b52a
maitatduy <maitatduy.work@gmail.com>
2026-02-22 12:05:36 +0700

    feat: add revenue chart

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -30,3 +30,58 @@ module.exports.dashboard = async (req, res) => {
         overview: overview,
     });
 };
+
+module.exports.revenueChartPost = async (req, res) => {
+    const { currentMonth, currentYear, previousMonth, previousYear, arrayDay } =
+        req.body;
+
+    // Truy vấn tất cả đơn hàng trong tháng hiện tại
+    const ordersCurrentMonth = await Order.find({
+        deleted: false,
+        createdAt: {
+            $gte: new Date(currentYear, currentMonth - 1, 1),
+            $lt: new Date(currentYear, currentMonth, 1),
+        },
+    });
+
+    // Truy vấn tất cả đơn hàng trong tháng trước
+    const ordersPreviousMonth = await Order.find({
+        deleted: false,
+        createdAt: {
+            $gte: new Date(previousYear, previousMonth - 1, 1),
+            $lt: new Date(previousYear, previousMonth, 1),
+        },
+    });
+
+    // Tạo mảng doanh thu theo từng ngày
+    const dataMonthCurrent = [];
+    const dataMonthPrevious = [];
+
+    for (const day of arrayDay) {
+        // Tính tổng doanh thu theo từng ngày của tháng này
+        let totalCurrent = 0;
+        for (const order of ordersCurrentMonth) {
+            const orderDate = new Date(order.createdAt).getDate();
+            if (day == orderDate) {
+                totalCurrent += order.total;
+            }
+        }
+        dataMonthCurrent.push(totalCurrent);
+
+        // Tính tổng doanh thu theo từng ngày của tháng trước
+        let totalPrevious = 0;
+        for (const order of ordersPreviousMonth) {
+            const orderDate = new Date(order.createdAt).getDate();
+            if (day == orderDate) {
+                totalPrevious += order.total;
+            }
+        }
+        dataMonthPrevious.push(totalPrevious);
+    }
+
+    res.json({
+        code: "success",
+        dataMonthCurrent: dataMonthCurrent,
+        dataMonthPrevious: dataMonthPrevious,
+    });
+};
--- routes/admin/dashboard.route.js
+++ routes/admin/dashboard.route.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const dashboardController = require("../../controllers/admin/dashboard.controller");
 
 router.get("/", dashboardController.dashboard);
+router.post("/revenue-chart", dashboardController.revenueChartPost);
 
 module.exports = router;
