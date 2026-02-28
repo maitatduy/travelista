7960ab6b05f37251f59d1522080c8ad880657bcd
maitatduy <maitatduy.work@gmail.com>
2026-02-28 23:43:51 +0700

    fix: update email helper

--- helpers/mail.helper.js
+++ helpers/mail.helper.js
@@ -4,13 +4,19 @@ module.exports.sendMail = (email, subject, content) => {
     const secure = process.env.EMAIL_SECURE === "true";
 
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: secure, // http: false, https: true
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD,
-        }
+        },
+        connectionTimeout: 10000,
+        greetingTimeout: 10000,
+        socketTimeout: 10000,
+        tls: {
+            rejectUnauthorized: false,
+        },
     });
 
     const mailOptions = {
